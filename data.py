2ff4d1e52eb13b0bcd70ba255919604ccefd35fc
Soham Taneja` <43928619+McAmbush@users.noreply.github.com>
2019-03-20 11:50:19 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,16 @@
+import sklearn
+import sklearn.datasets
+import numpy as np
+import matplotlib.pyplot as plt
+def load_dataset():
+    np.random.seed(1)
+    train_X, train_Y = sklearn.datasets.make_circles(n_samples=300, noise=.05)
+    np.random.seed(2)
+    test_X, test_Y = sklearn.datasets.make_circles(n_samples=100, noise=.05)
+    # Visualize the data
+    plt.scatter(train_X[:, 0], train_X[:, 1], c=train_Y, s=40, cmap=plt.cm.Spectral);
+    train_X = train_X.T
+    train_Y = train_Y.reshape((1, train_Y.shape[0]))
+    test_X = test_X.T
+    test_Y = test_Y.reshape((1, test_Y.shape[0]))
+    return train_X, train_Y, test_X, test_Y
